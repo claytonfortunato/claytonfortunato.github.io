7b2f6c874ad6c20346b000261dcc38bc945b91c3
claytonfortunato <claytonreis10@gmail.com>
2023-01-27 19:45:04 -0300

    feat: navbar responsive

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
     strings: "Clayton Fortunato",
   }).go();
 });
+
+const ul = document.querySelector(".nav-itens");
+const menuBtn = document.querySelector(".menu-btn i");
+
+const menuShow = () => {
+  if (ul.classList.contains("open")) {
+    ul.classList.remove("open");
+  } else {
+    ul.classList.add("open");
+  }
+};
+
+menuBtn.addEventListener("click", () => {
+  menuShow();
+});
