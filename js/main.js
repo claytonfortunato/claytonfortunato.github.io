4225c735174cc405973a072beea4c31af8b5f87c
claytonfortunato <claytonreis10@gmail.com>
2023-02-26 23:09:25 -0300

    feat: add modal no projeto

--- js/main.js
+++ js/main.js
@@ -41,3 +41,17 @@ animeScroll();
 window.addEventListener("scroll", () => {
   animeScroll();
 });
+
+//Modal
+const openModalButton = document.querySelector("#open-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
