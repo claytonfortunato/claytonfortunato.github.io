9ecd17409d7852a7924a119d048501278e28bab0
claytonfortunato <claytonreis10@gmail.com>
2023-03-03 11:16:51 -0300

    Refactor: mudança em codigo

--- js/modal.js
+++ js/modal.js
@@ -30,7 +30,7 @@ const dataProjects = [
       • Adicionando ou diminuindo quantidade de produto; <br>
       • Removendo produto de carrinho; <br>
       • Marcando o total do valor de produto no carrinho; <br>
-      • Responsividade.<br>
+      • Responsividade.<br><br>
       Tecnologias usadas no projetos:<br>
       • React<br>
       • Redux<br>
@@ -40,6 +40,68 @@ const dataProjects = [
       "https://user-images.githubusercontent.com/104373308/216209688-b26e39f1-5b9d-416d-a4a2-7930bb502720.mp4",
     repository: "https://github.com/claytonfortunato/e-commerce",
     site: "https://iridescent-maamoul-4f2fd4.netlify.app/",
+  },
+  {
+    title: "Forecast Weather",
+    date: "18 de Fevereiro de 2023",
+    description: `Essa aplicação web tem o intuito de buscar e mostrar a temperatura de cada cidade buscada. <br><br>
+    
+    • Busca de cidade; <br>
+    • Mostrando clima de cada cidade; <br>
+    • Horário de inicio e término do por-do-sol; <br>
+    • Velocidade e direção do vento; <br>
+    • Radar meteorológico e mapas de chuva; <br>
+    • Humidade do ar; <br>
+    
+    <br>
+    Tecnologias usadas no projetos:<br>
+    • React<br>
+    • Typescript<br>
+    • Styled-Components
+    `,
+    videoSrc:
+      "https://user-images.githubusercontent.com/104373308/220668453-df083966-c84d-42a5-b8cd-96a634c5c881.mp4",
+    repository: "https://github.com/claytonfortunato/weather-app",
+    site: "",
+  },
+  {
+    title: "Jogo da Memória",
+    date: "22 de Fevereiro de 2023",
+    description: `Um jogo da memória, onde os usuários devem lembrar as posições das cartas e abrir duas imagens exatas sequencialmente para marcar pontos. Uma vez que todos os cartões são abertos, o usuário é vitorioso. <br><br>
+    
+    • Rastrear os movimentos; <br>
+    • Pontos de acertos; <br>
+    • Função caso ganhe; <br>
+    • Resetar funcionalidade; <br>
+    
+    <br>
+    Tecnologias usadas no projetos:<br>
+    • React<br>
+    • Typescript<br>
+    • Styled-Components
+    `,
+    videoSrc:
+      "https://user-images.githubusercontent.com/104373308/221963518-22130a40-91ff-4998-b87a-a5178747807c.mp4",
+    repository: "https://github.com/claytonfortunato/MemoryGame",
+    site: "https://eloquent-rugelach-091b24.netlify.app/",
+  },
+  {
+    title: "Calculadora",
+    date: "17 de Fevereiro de 2023",
+    description: `Essa aplicação web tem o intuito de demonstrar uma simples calculadora. <br><br>
+      
+      • Adicionar elemento; <br>
+      • Calcular elemento; <br>
+      
+      <br>
+      Tecnologias usadas no projetos:<br>
+      • React<br>
+      • SASS
+      `,
+    videoSrc:
+      "https://user-images.githubusercontent.com/104373308/219822585-44b4c6e5-a9c0-43aa-8b52-5414cc789fc7.mp4",
+    repository: "https://github.com/claytonfortunato/Calculator-React",
+    site: "https://extraordinary-cat-761efb.netlify.app/",
   },
   {
     title: "Movies",
@@ -80,68 +142,6 @@ const dataProjects = [
       "https://user-images.githubusercontent.com/104373308/219694885-2e7161e8-ddd1-4f91-b370-a34a9c2b56f4.mp4",
     repository: "https://github.com/claytonfortunato/Todo-List",
     site: "",
-  },
-  {
-    title: "Calculadora",
-    date: "17 de Fevereiro de 2023",
-    description: `Essa aplicação web tem o intuito de demonstrar uma simples calculadora. <br><br>
-      
-      • Adicionar elemento; <br>
-      • Calcular elemento; <br>
-      
-      <br>
-      Tecnologias usadas no projetos:<br>
-      • React<br>
-      • SASS
-      `,
-    videoSrc:
-      "https://user-images.githubusercontent.com/104373308/219822585-44b4c6e5-a9c0-43aa-8b52-5414cc789fc7.mp4",
-    repository: "https://github.com/claytonfortunato/Calculator-React",
-    site: "https://extraordinary-cat-761efb.netlify.app/",
-  },
-  {
-    title: "Forecast Weather",
-    date: "18 de Fevereiro de 2023",
-    description: `Essa aplicação web tem o intuito de buscar e mostrar a temperatura de cada cidade buscada. <br><br>
-      
-      • Busca de cidade; <br>
-      • Mostrando clima de cada cidade; <br>
-      • Horário de inicio e término do por-do-sol; <br>
-      • Velocidade e direção do vento; <br>
-      • Radar meteorológico e mapas de chuva; <br>
-      • Humidade do ar; <br>
-      
-      <br>
-      Tecnologias usadas no projetos:<br>
-      • React<br>
-      • Typescript<br>
-      • Styled-Components
-      `,
-    videoSrc:
-      "https://user-images.githubusercontent.com/104373308/219822585-44b4c6e5-a9c0-43aa-8b52-5414cc789fc7.mp4",
-    repository: "https://github.com/claytonfortunato/weather-app",
-    site: "",
-  },
-  {
-    title: "Jogo da Memória",
-    date: "22 de Fevereiro de 2023",
-    description: `Um jogo da memória, onde os usuários devem lembrar as posições das cartas e abrir duas imagens exatas sequencialmente para marcar pontos. Uma vez que todos os cartões são abertos, o usuário é vitorioso. <br><br>
-      
-      • Rastrear os movimentos; <br>
-      • Pontos de acertos; <br>
-      • Função caso ganhe; <br>
-      • Resetar funcionalidade; <br>
-      
-      <br>
-      Tecnologias usadas no projetos:<br>
-      • React<br>
-      • Typescript<br>
-      • Styled-Components
-      `,
-    videoSrc:
-      "https://user-images.githubusercontent.com/104373308/221963518-22130a40-91ff-4998-b87a-a5178747807c.mp4",
-    repository: "https://github.com/claytonfortunato/MemoryGame",
-    site: "https://eloquent-rugelach-091b24.netlify.app/",
   },
 ];
 
